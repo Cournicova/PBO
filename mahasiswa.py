6ece49a949d0b50a44b59f61ee8b15bc4bcf1782
Cournicova <127724462+Cournicova@users.noreply.github.com>
2023-03-14 23:46:59 +0700

    Add files via upload

---
+++ mahasiswa.py
@@ -0,0 +1,31 @@
+class Mahasiswa:
+    def __init__(self, nama, umur, jurusan):
+        self.nama = nama
+        self.umur = umur
+        self.jurusan = jurusan
+    
+    def get_info(self):
+        print(f"Nama: {self.nama}")
+        print(f"Umur: {self.umur}")
+        print(f"Jurusan: {self.jurusan}")
+    
+    def set_nama(self, nama_baru):
+        self.nama = nama_baru
+    
+    def set_umur(self, umur_baru):
+        self.umur = umur_baru
+
+mhs1 = Mahasiswa("Firza Firdaus", 20, "Pendidikan Teknik Informatika")
+mhs2 = Mahasiswa("Ibnu Habib", 19, "Pendidikan Bahasa Inggris")
+mhs3 = Mahasiswa("Miko Abraham", 20, "Pendidikan Dokter")
+
+mhs1.get_info()
+print("-"*40)
+mhs2.get_info()
+print("-"*40)
+mhs3.get_info()
+print("-"*40)
+mhs2.set_nama("Saghofa Bagaskara")
+mhs2.set_umur(20)
+print("-"*40)
+mhs2.get_info()
