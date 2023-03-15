9e69e6160b7dd9a18adf31a5c2199bc2d4d504aa
Cournicova <127724462+Cournicova@users.noreply.github.com>
2023-03-15 07:36:40 +0700

    Update mahasiswa.py

--- mahasiswa.py
+++ mahasiswa.py
@@ -15,17 +15,17 @@ class Mahasiswa:
     def set_umur(self, umur_baru):
         self.umur = umur_baru
 
-mhs1 = Mahasiswa("Firza Firdaus", 20, "Pendidikan Teknik Informatika")
-mhs2 = Mahasiswa("Ibnu Habib", 19, "Pendidikan Bahasa Inggris")
-mhs3 = Mahasiswa("Miko Abraham", 20, "Pendidikan Dokter")
+mhs1 = Mahasiswa("Cournicova", 20, "Pendidikan Teknik Informatika")
+mhs2 = Mahasiswa("Afiffah", 19, "Pendidikan Bahasa Inggris")
+mhs3 = Mahasiswa("Syailendra", 20, "Pendidikan Dokter")
 
 mhs1.get_info()
 print("-"*40)
 mhs2.get_info()
 print("-"*40)
 mhs3.get_info()
-print("-"*40)
-mhs2.set_nama("Saghofa Bagaskara")
+print()
+mhs2.set_nama("Maharani")
 mhs2.set_umur(20)
 print("-"*40)
 mhs2.get_info()
